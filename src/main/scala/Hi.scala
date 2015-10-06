e394ec4d3a412f4498e4e5d463e6b2b6a50b07ae
taivop <taivo.pungas@gmail.com>
2015-10-06 19:05:42 +0200

    test crawler report

--- src/main/scala/Hi.scala
+++ src/main/scala/Hi.scala
@@ -1,11 +1,11 @@
-import main.scala.LanguageDetector
+package main.scala
 
 object Hi {
   def main(args: Array[String]) = {
     println("Hi!")
-
     println(LanguageDetector.detect("das ist mein Vater"))
-
     println(LanguageDetector.detect("I like this bowl of soup"))
+
+    Crawler.report()
   }
 }
