7bf7def77bb58db2de624c43b5a527a115eefe06
taivop <taivo.pungas@gmail.com>
2015-10-06 18:29:27 +0200

    documentation

--- src/main/scala/Tokenizer.scala
+++ src/main/scala/Tokenizer.scala
@@ -3,6 +3,7 @@ package main.scala
 object Tokenizer {
   type Token = String
 
+  /** Tokenize a document */
   def tokenize(doc: String): Iterator[Token] = {
     doc.toLowerCase().replaceAll("\\s", " ").replaceAll("[^a-z -]", "").sliding(3)
   }
