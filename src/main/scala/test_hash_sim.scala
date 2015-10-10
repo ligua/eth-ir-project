22fd4e796ed501646890374bdf86c96f13d0273c
KarimLabib <karim.rushdi@gmail.com>
2015-10-10 12:22:36 +0200

    Better test case

--- src/main/scala/test_hash_sim.scala
+++ src/main/scala/test_hash_sim.scala
@@ -8,7 +8,8 @@ object test_hash_sim {
     
     val ss = SimilarityDetector
     
-    
+    var x = (false,false)
+    /*
     var x = ss.isSimilarOrDuplicate("Are the variables mutable in Java? If yes (else they should be final in Java): Would it make sense to make them immutable in Scala?")
     println(x)
     
@@ -26,6 +27,26 @@ object test_hash_sim {
     
     x = ss.isSimilarOrDuplicate("Shakespeare produced most of his work after 1589")
     println(x)
+    * 
+    *
+    */
+    
+    val main_obj = Main_Object
+    
+    val url1 = "http://idvm-infk-hofmann03.inf.ethz.ch/eth/www.ethz.ch/en.html"
+    var(text1, links1) = Main_Object.getTextAndLinksFromUrl(url1)
+    
+    val url2 = "http://idvm-infk-hofmann03.inf.ethz.ch/eth/www.ethz.ch/en.html"
+    var(text2, links2) = Main_Object.getTextAndLinksFromUrl(url2)
+    
+    x = ss.isSimilarOrDuplicate(text1.mkString(" "))
+    println(x)
+    
+    text2 = text2.map{case "ETH" => "EPFL"; case x => x}
+
+    x = ss.isSimilarOrDuplicate(text2.mkString(" "))
+    println(x)
+    val doc = Main_Object
     
   }
 }
