373cebc1337092d184b2574235d23552a6135a9b
KarimLabib <karim.rushdi@gmail.com>
2015-11-30 03:16:00 +0100

    Further optimization for second pass

--- p2/src/main/scala/Main.scala
+++ p2/src/main/scala/Main.scala
@@ -1,24 +1,62 @@
 package main.scala
 
+import java.io._
+
 object Main {
 
   type Features = Array[Array[Double]]
   type Labels   = Array[Int]
+  type PredictedRelevancyProbability = Array[Double]
+
+  class FeatureArrayWithRelevancy(docName: String, featArr: Array[Double], relProb: Double)
+  {
+    val correspondingDoc = docName
+    val featureArray = featArr
+
+    val relevancyProbability = relProb
+  }
 
   def testStream() = {
 
     val extracted = FeatureExtractor.extract_features()
 
-    val features = extracted._1
-    val labels = extracted._2
+    val featuresForTraining = extracted._1
+    val labelsForTraining = extracted._2
 
     // Perform classification using Random Forest from Weka ML library
-    Classifier.train(features, labels)
+    Classifier.train(featuresForTraining, labelsForTraining)
 
-    val predictedLabels = Classifier.predict(features)
-    labels.zip(predictedLabels).foreach(kv => println(s"true\t${kv._1}, predicted\t${kv._2}"))
 
-    println(s"F1 score: ${Classifier.eval_f1score(labels, predictedLabels)}")
+
+    // get 100 best features for each topic
+
+    val best1000FeaturesForTopics = FeatureExtractor.best1000FeaturesForRanking
+
+    var topic_counter = 0
+
+    val (predictedLabels, predictedRelevancyProbability) = Classifier.predict(featuresForTraining)
+
+    labelsForTraining.zip(predictedLabels).foreach(kv => println(s"true\t${kv._1}, predicted\t${kv._2}"))
+
+    println(s"F1 score: ${Classifier.eval_f1score(labelsForTraining, predictedLabels)}")
+
+
+    for(best1000FeatureForTopic <- FeatureExtractor.best1000FeaturesForRanking)
+      {
+        val tmpArrayOf1000BestFeatures = best1000FeatureForTopic.toArray.map(p => p.featureArray)
+        val tmpDocumentNamesOf1000BestFeatures = best1000FeatureForTopic.toArray.map(p => p.correspondingDoc)
+
+        val (predictedLabels, predictedRelevancyProbability) = Classifier.predict(tmpArrayOf1000BestFeatures)
+
+
+        val writer = new PrintWriter(new File("query"+(51 + topic_counter)+"_top_100.txt" ))
+
+        tmpDocumentNamesOf1000BestFeatures.zip(predictedRelevancyProbability).sortWith(_._2 > _._2).take(100).foreach(writer.println)
+
+        writer.close()
+
+        topic_counter += 1
+      }
   }
 
   def main(args: Array[String]): Unit = {
--- p2/src/main/scala/ch/ethz/dal/tinyir/processing/Tokenizer.scala
+++ p2/src/main/scala/ch/ethz/dal/tinyir/processing/Tokenizer.scala
@@ -2,5 +2,5 @@ package ch.ethz.dal.tinyir.processing
 
 object Tokenizer {
   def tokenize (text: String) : List[String] =
-    text.split("[ .,;:?!\t\n\r\f]+").toList
+    text.split("[ \"/.,-;&:(?)!\t\n\r\f]+").toList
 }
