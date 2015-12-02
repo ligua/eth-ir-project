079e6a4d08cf655d18494d6e078b51885cd3235f
taivop <taivo.pungas@gmail.com>
2015-12-02 13:38:19 +0100

    script to get collection status

---
+++ p2/Results/get_collection_stats.sh
@@ -0,0 +1,31 @@
+# Change the folder name to get stats files from a different folder
+folder=Dirichlet_24percent_12percent
+
+cd $folder
+
+echo "------- Language-based model --------"
+# What it does: find lines with numbers | cut out correct field | remove NaNs | calculate average | format and print
+printf "Mean precision:\t\t\t"
+grep "^Precision = " statistics_language_based.txt | cut -d" " -f3 | grep "^\d" | awk '{s+=$1}END{print s/NR}' RS="\n" | xargs printf "%.3f\n"
+printf "Mean recall:\t\t\t"
+grep "^Recall = " statistics_language_based.txt | cut -d" " -f3 | grep "^\d" | awk '{s+=$1}END{print s/NR}' RS="\n" | xargs printf "%.3f\n"
+printf "Mean F1-score:\t\t\t"
+grep "^F1-score = " statistics_language_based.txt | cut -d" " -f3 | grep "^\d" | awk '{s+=$1}END{print s/NR}' RS="\n" | xargs printf "%.3f\n"
+printf "Mean average precision:\t\t"
+grep "^Average Precision = " statistics_language_based.txt | cut -d" " -f4 | grep "^\d" | awk '{s+=$1}END{print s/NR}' RS="\n" | xargs printf "%.3f\n"
+echo "-------------------------------------"
+
+printf "\n"
+
+echo "------ Machine learning model -------"
+printf "Mean precision:\t\t\t"
+grep "^Precision = " statistics_term_based.txt | cut -d" " -f3 | grep "^\d" | awk '{s+=$1}END{print s/NR}' RS="\n" | xargs printf "%.3f\n"
+printf "Mean recall:\t\t\t"
+grep "^Recall = " statistics_term_based.txt | cut -d" " -f3 | grep "^\d" | awk '{s+=$1}END{print s/NR}' RS="\n" | xargs printf "%.3f\n"
+printf "Mean F1-score:\t\t\t"
+grep "^F1-score = " statistics_term_based.txt | cut -d" " -f3 | grep "^\d" | awk '{s+=$1}END{print s/NR}' RS="\n" | xargs printf "%.3f\n"
+printf "Mean average precision:\t\t"
+grep "^Average Precision = " statistics_term_based.txt | cut -d" " -f4 | grep "^\d" | awk '{s+=$1}END{print s/NR}' RS="\n" | xargs printf "%.3f\n"
+echo "-------------------------------------"
+
+cd ..
